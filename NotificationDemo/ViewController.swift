ab95d83e423a7e55e172621210c6876b0f3f36bb
Allen <Allen>
2016-10-19 20:33:24 +0800

    add showNotification IBAction func

--- NotificationDemo/ViewController.swift
+++ NotificationDemo/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
 
     let myNotification = Notification.Name(rawValue: "MyNotification")
 
+    @IBAction func showNotification(_ sender: AnyObject) {
+        
+        let nc = NotificationCenter.default
+        
+        nc.post(name: myNotification, object: nil, userInfo:["message": "您已到達運動用品區,目前公路車全面八折優惠中!"])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
